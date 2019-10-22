434eea30ace8a32429b35d0f5f3be6087823763a
Cole Rau <colevrau@gmail.com>
2019-10-22 06:21:03 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,8 +47,26 @@ def naming_system
   {
   label: "Kingdom",
   sub_category: {
-    
+    label: "Phylum",
+    sub_vategory: {
+      label: "Class",
+      sub_category: {
+        label: "Order",
+        sub_category: {
+          label: "Family",
+          sub_category: {
+            label: "Genus",
+            sub_category: {
+              label: "Species"
+              sub_category: {
+                nil
+              }
+            }
+          }
+        }
+      }
+    }
   }
-
+}
 
 end
